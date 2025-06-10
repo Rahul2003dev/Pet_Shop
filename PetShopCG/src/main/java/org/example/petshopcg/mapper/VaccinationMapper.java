7aac3f41e50ff0eab45ea7a31453ced26475f9e4
Harsimran Kaur <harsimran0596.be21@chitkara.edu.in>
2025-06-10 14:53:30 +0530

    [Harsimran] update : updated by adding detailed comments to Vaccination Mapper

--- PetShopCG/src/main/java/org/example/petshopcg/mapper/VaccinationMapper.java
+++ PetShopCG/src/main/java/org/example/petshopcg/mapper/VaccinationMapper.java
@@ -4,9 +4,13 @@ import org.example.petshopcg.dto.VaccinationDto;
 import org.example.petshopcg.entity.Vaccination;
 import org.mapstruct.Mapper;
 
+// This interface uses MapStruct to automatically generate mapping code at build time
 @Mapper(componentModel = "spring")
 public interface VaccinationMapper {
+
+    // Converts Vaccination entity to VaccinationDto
     VaccinationDto toDto(Vaccination entity);
+
+    // Converts VaccinationDto to Vaccination entity
     Vaccination toEntity(VaccinationDto dto);
 }
-
