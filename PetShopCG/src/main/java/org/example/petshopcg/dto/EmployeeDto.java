52b534889a0e9486b9753abc200263d6f376b724
Harsimran Kaur <harsimran0596.be21@chitkara.edu.in>
2025-06-10 14:52:43 +0530

    [Harsimran] update : updated by adding detailed comments to Employee Dto

--- PetShopCG/src/main/java/org/example/petshopcg/dto/EmployeeDto.java
+++ PetShopCG/src/main/java/org/example/petshopcg/dto/EmployeeDto.java
@@ -7,18 +7,25 @@ import org.example.petshopcg.entity.Employee;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-@Value
+@Value // Makes the class immutable and generates getters, toString, equals, and hashCode
 public class EmployeeDto implements Serializable {
-    Integer id;
-    @Size(max = 255)
-    String firstName;
-    @Size(max = 255)
-    String lastName;
-    @Size(max = 255)
-    String position;
-    LocalDate hireDate;
-    @Size(max = 255)
-    String phoneNumber;
-    @Size(max = 255)
-    String email;
+
+    Integer id; // Unique ID of the employee
+
+    @Size(max = 255) // Limits the length of first name to 255 characters
+    String firstName; // First name of the employee
+
+    @Size(max = 255) // Limits the length of last name to 255 characters
+    String lastName; // Last name of the employee
+
+    @Size(max = 255) // Limits the length of position to 255 characters
+    String position; // Job position/title of the employee
+
+    LocalDate hireDate; // Date the employee was hired
+
+    @Size(max = 255) // Limits the length of phone number to 255 characters
+    String phoneNumber; // Contact number of the employee
+
+    @Size(max = 255) // Limits the length of email to 255 characters
+    String email; // Email address of the employee
 }
