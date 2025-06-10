d1bdfa4b5e29615dbc4841e3c7b63582fda50ea2
Harsimran Kaur <harsimran0596.be21@chitkara.edu.in>
2025-06-10 14:54:26 +0530

    [Harsimran] update : updated by adding detailed comments to Employee Repository

--- PetShopCG/src/main/java/org/example/petshopcg/repository/EmployeeRepository.java
+++ PetShopCG/src/main/java/org/example/petshopcg/repository/EmployeeRepository.java
@@ -2,12 +2,20 @@ package org.example.petshopcg.repository;
 
 import org.example.petshopcg.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * Repository interface for Employee entity.
+ * Extends JpaRepository to provide basic CRUD operations and custom query methods.
+ */
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+    // Finds a list of employees with the given first name.
     List<Employee> findByFirstName(String firstName);
 
+    // Finds a list of employees with the given position
     List<Employee> findByPosition(String position);
 }
