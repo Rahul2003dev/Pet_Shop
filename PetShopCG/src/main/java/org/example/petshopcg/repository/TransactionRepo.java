d4ecb8c38d1aeb36f4eaa50b6133dff43da3859f
Manjot Singh <Manjot Singh>
2025-06-11 10:01:25 +0530

    Manjot Updated Transaction Repository.

--- PetShopCG/src/main/java/org/example/petshopcg/repository/TransactionRepo.java
+++ PetShopCG/src/main/java/org/example/petshopcg/repository/TransactionRepo.java
@@ -4,7 +4,6 @@ import org.example.petshopcg.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface TransactionRepo extends JpaRepository<Transaction, Integer> {
